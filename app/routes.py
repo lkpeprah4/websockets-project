e94313acf78224da194ef389836c2108c4fb8d22
kwanimaa <akousakwanimaaaa@gmail.com>
2026-05-22 11:40:32 +0000

    REST API endpoints complete and tested

--- app/routes.py
+++ app/routes.py
@@ -7,17 +7,34 @@ rooms_bp = Blueprint("rooms", __name__)
 @rooms_bp.route("/rooms", methods=["POST"])
 def create_room():
     data=request.get_json()
+    name=data.get("name")
 
-    if not data or not data.get("name"):
+    if not name:
         return jsonify ({"error":"ROOM NAME IS REQUIRED"}),400
     
-    existing_room = Room.query.filter_by(name=data["name"]).first()
+    existing_room = Room.query.filter_by(name=name).first()
     if existing_room:
         return jsonify({"error": "Room already exists"}), 400
 
-    room = Room(name=data["name"])
+    room = Room(name=name)
     db.session.add(room)
     db.session.commit()
 
-    return jsonify(room.to_dict()), 201
+    return jsonify({"msg":"ROOM CREATED SUCESSFULLY","room":room.to_dict()}), 201
+
+
+@rooms_bp.route("/rooms", methods=["GET"])
+def get_rooms():
+    rooms=Room.query.all()
+    return jsonify({"rooms":[room.to_dict() for room in rooms]}),200
+
+
+@rooms_bp.route("/rooms/<string:name>/messages", methods=["GET"])
+def get_messages(name):
+    room=Room.query.filter_by(name=name).first()
+
+    if not room:
+        return jsonify({"msg":"ROOM NOT FOUND"}),404
     
+    messages=Message.query.filter_by(room_id=room.id).all()
+    return jsonify({"messages":[message.to_dict() for message in messages]}),200
